b7669e9729aa27765cb8cec56828186d71a62295
njohdesign <njohtah@gmail.com>
2026-09-14 09:52:45 +0100

    Use current Amazon Linux 2023 AMI

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -2,8 +2,33 @@ provider "aws" {
   region = "us-east-1"
 }
 
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.*-x86_64"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = data.aws_ami.amazon_linux.id
   instance_type = "t2.micro"
 
   tags = {
